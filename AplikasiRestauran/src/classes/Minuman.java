3415b65363540cc4c344261b07c7100376f19144
unknown <minaryanshofa1@gmail.com>
2021-02-10 08:40:39 +0700

    Implementasi Struktur kelas aplikasi kasir

---
+++ AplikasiRestauran/src/classes/Minuman.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author A409FJ
+ */
+public class Minuman extends Menu {
+    
+    public Minuman(String nama_minuman, double harga) {}
+    
+}
