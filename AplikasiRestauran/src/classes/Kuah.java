2bbda25312e9e9d34e2a37beb63d0f5dcc834df6
unknown <minaryanshofa1@gmail.com>
2021-02-24 01:17:13 +0700

    Implementasi Kelas Ramen, Kuah, Toping dan Minuman

--- AplikasiRestauran/src/classes/Kuah.java
+++ AplikasiRestauran/src/classes/Kuah.java
@@ -11,6 +11,10 @@ package classes;
  */
 public class Kuah extends Menu {
     
-    public Kuah(String nama_kuah) { }
+    public Kuah(String nama_kuah) {
+        setNama_menu(nama_kuah);
+        setHarga(0);
+        setKategori("kuah");
+    }
     
 }
